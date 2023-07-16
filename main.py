342eaf7015c1f1ee57d6a29e40fdf969457af0d2
nmettke <mettke@umich.edu>
2023-07-16 17:42:02 -0400

    Webscraping tutorial

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-# This is a sample Python script.
+from urllib.request import urlopen
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if '__main__':
-    print_hi('PyCharm')
-    print('blah asda')
+    url = "http://olympus.realpython.org/profiles/aphrodite"
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    print(html)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
