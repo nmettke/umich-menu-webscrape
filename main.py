d387c6a0f0abf974b42756ce5abe0dd117a47141
nmettke <mettke@umich.edu>
2023-07-16 17:31:06 -0400

    first commit

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if '__main__':
     print_hi('PyCharm')
-    print('blah')
+    print('blah asda')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
