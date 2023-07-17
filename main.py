9e85df8c7764422a12d4d6ea763206fe1fc1c30a
nmettke <mettke@umich.edu>
2023-07-16 21:22:21 -0400

    Added twilio functionality

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import date
+import twilio
 
 if '__main__':
-    url = "http://olympus.realpython.org/profiles/aphrodite"
+    # Date information
+    today = date.today()
+    d2 = today.strftime("%B %d, %Y")
+
+    url = "https://dining.umich.edu/menus-locations/dining-halls/south-quad/"
     page = urlopen(url)
-    html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
-    print(html)
-    print("fuzz")
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+
+    fname = "south_quad.txt"
+    with open(fname, "w", encoding="utf-8") as f:
+        f.write(html)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ send-sms.py
@@ -0,0 +1,15 @@
+import twilio
+
+from twilio.rest import Client
+
+account_sid = 'ACa1b99017fefaa4d838076cfb209bc9b2'
+auth_token = 'bdb9b47df85af70ccf304b3eaa12d03b'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='+18559064470',
+  body='Suck my cock chance',
+  to='+17342318482'
+)
+
+print(message.sid)
