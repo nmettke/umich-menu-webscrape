4bfbf67c7fdc29dc6354a5d5826665398f7c5d43
nmettke <mettke@umich.edu>
2023-07-17 16:07:57 -0400

    Webscraped from all dining halls, will add functionality to limit website calls

--- main.py
+++ main.py
@@ -7,23 +7,34 @@ if __name__ == '__main__':
     today = date.today()
     d2 = today.strftime("%B %d, %Y")
 
-    fname = "south_quad.txt"
-    url = "https://dining.umich.edu/menus-locations/dining-halls/south-quad/"
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    div = soup.find('div', {'id': 'mdining-items'})
-    meal_items = div.find_all('div', class_='courses')
+    dining_halls = ["south-quad", "twigs-at-oxford", "north-quad", "mosher-jordan", "markley", "east-quad", "bursley"]
+    not_serving = set()
+    url = "https://dining.umich.edu/menus-locations/dining-halls/"
+    for hall in dining_halls:
+        page = urlopen(url + hall)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        div = soup.find('div', {'id': 'mdining-items'})
+        no_menu = div.find('p', class_='no-menu')
+        if no_menu is not None:
+            not_serving.add(hall)
+            continue
+        else:
+            meal_items = div.find_all('div', class_='courses')
+            for tag in meal_items:
+                meal = tag.find_previous_sibling('h3')
+                items = tag.find_all('div', class_='item-name')
+                meal_name = meal.get_text()[1:]
+                fname = hall + '-' + meal_name
+                f = open(fname, "w", encoding="utf-8")
+                f.write(meal_name + " menu at " + hall + " on " + d2 + "\n")
+                for item in items:
+                    f.write(item.get_text() + "\n")
 
-    f = open(fname, "w", encoding="utf-8")
-    f.write("Michigan Dining menu for " + d2 + "\n")
-    for tag in meal_items:
-        meal = tag.find_previous_sibling('h3')
-        items = tag.find_all('div', class_='item-name')
-        meal_name = meal.get_text()[1:]
-        f.write(meal_name + "\n")
-        for item in items:
-            f.write(item.get_text() + "\n")
+    f = open("closed-halls", "w", encoding="utf-8")
+    for hall in not_serving:
+        f.write(hall + "\n")
+
 
 
 
