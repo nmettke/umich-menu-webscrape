ea7ec3c0d7f11d8404faf3d34e48b1cc524cc099
nmettke <mettke@umich.edu>
2023-07-19 17:45:14 -0400

    Finished functionality, slimmed code down

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
 from flask import Flask, request
-import requests
 from twilio.twiml.messaging_response import MessagingResponse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import date
+
+
+def menu_update():
+    today = date.today()
+    day = today.strftime("%B %d, %Y")
+
+    dining_halls = ["south-quad", "twigs-at-oxford", "north-quad", "mosher-jordan", "markley", "east-quad",
+                    "bursley"]
+    not_serving = set()
+    url = "https://dining.umich.edu/menus-locations/dining-halls/"
+    for hall in dining_halls:
+        page = urlopen(url + hall)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        div = soup.find('div', {'id': 'mdining-items'})
+        no_menu = div.find('p', class_='no-menu')
+        if no_menu is not None:
+            not_serving.add(hall)
+            continue
+        else:
+            meal_items = div.find_all('div', class_='courses')
+            for tag in meal_items:
+                meal = tag.find_previous_sibling('h3')
+                items = tag.find_all('div', class_='item-name')
+                meal_name = meal.get_text()[1:]
+                fname = hall + '-' + meal_name
+                f = open(fname, "w", encoding="utf-8")
+                f.write(meal_name + " menu at " + hall + " on " + day + "\n")
+                for item in items:
+                    f.write(item.get_text() + "\n")
+
+    f = open("closed-halls", "w", encoding="utf-8")
+    for hall in not_serving:
+        f.write(hall + "\n")
+
 
 app = Flask(__name__)
 
@@ -15,7 +52,17 @@ def bot():
     responded = False
     helpmsg = "Please enter the dining hall and the meal you want. Ex: \"South Quad Dinner\" " + "\n" + "Here is a list of dining halls: " + "\n" + "Bursley, East Quad, Markley, Mojo, North Quad, South Quad, Twigs"
 
-    print(incoming_msg)
+    today = date.today()
+    day = today.strftime("%B %d, %Y")
+
+    with open('date', 'r') as f:
+        data_date = f.readline()
+        print(data_date)
+
+    if data_date != day:
+        with open('date', 'w', encoding="utf-8") as f:
+            f.write(day)
+        menu_update()
 
     if 'help' in incoming_msg:
         # return a quote
@@ -40,6 +87,13 @@ def bot():
         msg.body("Please respond with a valid hall. Text help for more details.")
         return str(resp)
 
+    with open('closed-halls', 'r', encoding="utf-8") as f:
+        closed_halls = set(line.strip() for line in f)
+
+    if hall in closed_halls:
+        msg.body(f"{hall} is not serving food today.")
+        return str(resp)
+
     if 'breakfast' in incoming_msg:
         meal = "Breakfast"
     elif 'lunch' in incoming_msg:
@@ -52,8 +106,12 @@ def bot():
         msg.body("Please respond with a valid meal. Text help for more details.")
         return str(resp)
 
-    with open(hall + '-' + meal, 'r') as file:
-        data = file.read()
+    try:
+        with open(hall + '-' + meal, 'r') as file:
+            data = file.read()
+    except FileNotFoundError:
+        msg.body(f"{hall} is not serving {meal} today.")
+        return str(resp)
 
     msg.body(data)
 
--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ import requests
 
 if __name__ == '__main__':
     url = 'http://127.0.0.1:5000/bot'
-    payload = {'Body': 'south quad lunch'}
+    payload = {'Body': 'south quad brunch'}
     response = requests.post(url, data=payload)
 
     print(response.text)
